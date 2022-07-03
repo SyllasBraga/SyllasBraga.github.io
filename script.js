f91089339d99ac467cdede7019623b5639c2a629
SyllasBraga <syllasbraga2@gmail.com>
2022-07-03 19:29:09 -0300

    1

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
 const BTN_BACK = document.getElementById("back");
 const BTN_NEXT = document.getElementById("next");
-const ARRAY_IMG = [];
+const alterImg = document.getElementById("img");
+const ARRAY_IMG =  ["./images/cocacola1.png",
+                    "./images/cocacola2.png",
+                    "./images/cocacola3.png",
+                    "./images/cocacola4.png",
+                    "./images/cocacola5.png",
+                    "./images/cocacola6.png"];
+let cont = 0;
+  
+function passarImg() {
+        cont++;
+        if (cont >= ARRAY_IMG.length) {
+            cont = 0;
+            alterImg.src = ARRAY_IMG[cont];
+        }else{
+            alterImg.src = ARRAY_IMG[cont]; 
+        }
+}
+
+function voltarImg() {
+        cont--;
+        if (cont < 0) {
+            cont = ARRAY_IMG.length - 1;
+            alterImg.src = ARRAY_IMG[cont];       
+        }else{
+            alterImg.src = ARRAY_IMG[cont];               
+        }
+}
+
+BTN_NEXT.addEventListener('click', passarImg);
+BTN_BACK.addEventListener('click', voltarImg);
+
